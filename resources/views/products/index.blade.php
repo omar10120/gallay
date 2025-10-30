f80f0e65cb399f787de34f8d36ee36b550cfda1a
tamer tamer <amr.dawoodi@hotmail.com>
2025-10-30 16:42:06 +0300

    modiy diesng , fab-social new design

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -13,13 +13,13 @@
    
 
     @if($products->count() > 0)
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
             @foreach($products as $product)
                 <div class="" style=" ">
                     <div class="relative group m-2 rounded-xl overflow-hidden" style="background: rgba(0,0,0,0.08);">
                         <!-- <a href="{{ route('products.show', $product) }}" class="block"> -->
                             @if($product->image)
-                                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-[520px] object-fit" />
+                                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-[520px] md:h-[520px] object-fit" />
                             @else
                                 <div class="w-full h-[460px] bg-[color:var(--color-primary)]/30 flex items-center justify-center">
                                     <i class="fas fa-image text-4xl text-[color:var(--color-secondaryDark)]"></i>
